8e193621c63dbde4c6501a31163b5605b097a7bb
MatthewELucio <44351122+MatthewELucio@users.noreply.github.com>
2025-12-04 01:07:03 -0500

    day1 (cpp and rust)

---
+++ day1/day1.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int p1turn(int dial, bool isLeft, int distance) {
+    int M = 100;
+    if (isLeft) {
+        int res = (dial - distance) % M;
+        return (res < 0) ? (res + M) : res;
+    } else {
+        return (dial + distance) % M;
+    }
+}
+
+int p2turn(int dial, bool isLeft, int distance, int& numClicks) {
+    int M = 100;
+    numClicks = 0;
+    if (isLeft) {
+        int res = (dial - distance) % M;
+        numClicks = -(dial - distance) / M;
+        if (dial != 0 && res < 0) numClicks += 1;
+        return (res < 0) ? (res + M) : res;
+    } else {
+        numClicks = (dial + distance) / M;
+        if ((dial + distance) % M == 0) numClicks -= 1; 
+        return (dial + distance) % M;
+    }
+}
+
+int main() {
+    bool p1 = false;
+
+    int dial = 50;
+    int numDialZero = 0;
+    int numClicks = 0;
+
+    std::ifstream input("input.txt");
+    std::string line;
+
+    while (std::getline(input, line)) {
+        char dir = line[0];
+        int distance = std::stoi(line.substr(1, 3));
+
+        if (p1) {
+            dial = p1turn(dial, dir == 'L', distance);
+            numDialZero += dial == 0;
+        } else {
+            dial = p2turn(dial, dir == 'L', distance, numClicks);
+            numDialZero += dial == 0;
+            numDialZero += numClicks;
+        }
+    }
+    input.close();
+
+    std::cout << numDialZero << "\n";
+
+    return 0;
+}
---
+++ day1/rs/src/main.rs
@@ -0,0 +1,73 @@
+// use std::env;
+use std::fs;
+
+fn p1turn(dial: i32, is_left: bool, distance: i32) -> i32 {
+    const M: i32 = 100;
+    if is_left {
+        let res = (dial - distance) % M;
+        if res < 0 {
+            res + M
+        } else {
+            res
+        }
+    } else {
+        (dial + distance) % M
+    }
+}
+
+fn p2turn(dial: i32, is_left: bool, distance: i32) -> (i32, i32) {
+    const M: i32 = 100;
+    if is_left {
+        let res = (dial - distance) % M;
+        let mut num_clicks = -(dial - distance) / M;
+        if dial != 0 && res < 0 {
+            num_clicks += 1;
+        }
+        // println!("{} {} {}", dial, distance, res);
+        if res < 0 {
+            (res + M, num_clicks)
+        } else {
+            (res, num_clicks)
+        }
+    } else {
+        let mut num_clicks = (dial + distance) / M;
+        if ((dial + distance) % M) == 0 {num_clicks -= 1};
+        ((dial + distance) % M, num_clicks)
+    }
+    
+}
+
+fn main() {
+    let part1: bool = false;
+
+    let file_path = "../../input.txt";
+    let lines = fs::read_to_string(file_path).expect("can't read file");
+
+    let mut dial = 50;
+    let mut num_dial_zero = 0;
+
+    for line in lines.lines() {
+        let dir = &line[0..1];
+        let distance: i32 = (&line[1..]).trim().parse().expect("string could not be casted to int");
+
+        if part1 {
+            dial = p1turn(dial, dir == "L", distance);
+
+            if dial == 0 {
+                num_dial_zero += 1;
+            }
+        } else {
+            let (new_dial, num_clicks) = p2turn(dial, dir == "L", distance);
+            dial = new_dial;
+
+            if dial == 0 {
+                num_dial_zero += 1;
+            }
+
+            num_dial_zero += num_clicks;
+            // println!("{} {} {} {} {}", dir, distance, dial, num_clicks, num_dial_zero);
+        }
+    }
+
+    println!("{}", num_dial_zero);
+}
